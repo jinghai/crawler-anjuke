2c6f8b81b21e9e94894dbb20dce04b8021d6b366
jinghai <jinghai.xiao@gmail.com>
2017-01-20 16:22:28 +0800

    ok

--- demo/ClassDemo.js
+++ demo/ClassDemo.js
@@ -3,26 +3,32 @@
  */
 let SimpleCrawler = require("simplecrawler");
 
-
 let Crawler =(function(){
 
-
+  let _name = ""; //私有变量
   class Crawler extends SimpleCrawler {
 
     constructor(initialURL) {
       super(initialURL);
-
+      _name = "aaa";
     }
-
-
     stop(abortRequestsInFlight){
       super.stop(abortRequestsInFlight);
       this.emit("stop");
     }
 
+    get name(){
+      return _name;
+    }
+    set name(name){
+      _name = name;
+    }
 
+
+    static haha(){//静态方法只能在类上调用，实例中无此方法
+      console.log("haha")
+    }
   }
-
   return Crawler;
 })();
 
