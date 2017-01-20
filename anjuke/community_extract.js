9b4d9554e7e9b343f077579c16d01cd953d3470e
jinghai <jinghai.xiao@gmail.com>
2017-01-20 14:53:21 +0800

    ok

--- anjuke/community_extract.js
+++ anjuke/community_extract.js
@@ -13,20 +13,17 @@ var extractor = {
   helpUrl:/^http:\/\/shanghai\.anjuke\.com\/community(\/?)/i,
   schema: {
     url: String,
-    name: String,//市-区县-板块 组合名称（无区县、板块不组合）
-    area: String,//板块名称
-    district: String,//区/县
+    anjukeId:String,
+    name: String,
+    area: String,
+    district: String,
     city: String,
     year: String,
     month: String,
-    price: Number,//均价
-    mom: Number,//同比
-    momUpDown: String,//同比上升/下降
-    yoy: Number,//环比
-    yoyUpDown: String,//环比上升/下降
-    py: String,//拼音
-    letter: String,//拼音首字母
-    //point: Object,//坐标
+    price: Number,
+    mom: Number,
+    momUpDown: String,
+    point: Object,
   },
   //数据唯一标识(字段值连接后md5存放于__k)，若数据已存在不做更新
   keys: ["name", "year", "month"],
@@ -38,14 +35,55 @@ var extractor = {
   handler: function ($, queueItem, responseBuffer, response) {
 
 
+    //交集选择： $(".a.b")--选择同时包含a和b的元素。
+    //并集选择：$(".a, .b")--选择包含a或者包含b的元素。
+    //首页 > 上海小区 > 浦东小区 > 三林小区 > 浦发绿城
+    var str = $(".p_1180.p_crumbs").text().replace(/\s/g, '').split(">");
+    str.shift();
+    var city = str[0].replace(/小区/g, '');
+    var district = str[1].replace(/小区/g, '');
+    var area = str[2].replace(/小区/g, '');
+    var name = str[3];
+    //console.log(city,district,area,name);
+
+    //http://shanghai.anjuke.com/map/sale/?from=commtitle#l1=31.110616364328&l2=121.35934431231&l3=18&commid=1670&commname=xxx
+    var locationUrl = $(".comm-title.clearfix").find("a").attr("href").split("#")[1].split("&");
+    var lat = parseFloat(locationUrl[0].replace("l1=", ""));
+    var lng = parseFloat(locationUrl[1].replace("l2=", ""));
+    var anjukeId = locationUrl[3].replace("commid=", "");
+
+    //console.log(anjukeId,lat,lng);
+
+    var price = $("em.comm-avg-price").text();
+    //上涨0.52%
+    var mom = $(".price-tip>strong").eq(0).text().replace(/[^0-9.]/ig, "");
+    var momUpDown = $(".price-tip>strong").eq(0).hasClass("up") ? "up" : "down";
+    //console.log(price,momUpDown,mom);
+
+    var date = new Date();
+
+    var result = {
+      url: queueItem.url,
+      anjukeId:anjukeId,
+      name: name,
+      area: area,
+      district: district,
+      city: city,
+      year: date.getFullYear(),
+      month: date.getMonth() + 1,//getMonth 0~11
+      price: price,
+      mom: mom,
+      momUpDown: momUpDown,
+      point: {lat:lat,lng:lng},
+    }
 
     this.times++
     if (this.times > 0) {
-      //this.crawler.stop();
+      this.crawler.stop();
     }
     //console.log(regenTarget);
     console.log(this.times, queueItem.url);
-    //return result;
+    return result;
   }
 }
 
