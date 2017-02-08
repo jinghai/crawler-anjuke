28cee951eed9f20979a7a8efa29cda994b21b16a
jinghai <jinghai.xiao@gmail.com>
2017-02-08 12:26:33 +0800

    ok

--- anjuke/run.js
+++ anjuke/run.js
@@ -2,7 +2,5 @@
  * Created by yneos on 2017/1/11.
  */
 var MyCrawler =  require("../lib");
-var conf =  require("./conf.js");
-
-var cw = MyCrawler.createCrawler(conf);
+var cw = MyCrawler.createCrawler();
 cw.start();
