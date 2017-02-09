e331e8d293d2298af7c596298410e311fbc8212c
jinghai <jinghai.xiao@gmail.com>
2017-02-10 00:53:22 +0800

    ok

--- c_proxy/conf.js
+++ c_proxy/conf.js
@@ -5,11 +5,11 @@
 var conf = {
     //【秒】【分】【小时】【日】【月】【周】 *所有 ?不指定 -区间 */5每5
     cron: "00 00 08 * * *",//每日8:00
-    logLevel: 'debug',//debug|info|warn|error
+    logLevel: 'info',//debug|info|warn|error
     dbUrl: "mongodb://192.168.2.56/_crawler",//'mongodb://user:pass@localhost:port,anotherhost:port,yetanother:port/mydatabase'
     crawler: {
         url: "http://www.xicidaili.com",
-        interval: 1 * 1000 * 60 * 60 * 24 //间隔24小时
+        interval: 1 * 1000 * 60 * 60 * 1 //间隔1小时
     },
 
     extractors: [
