78b82437e52552c6d1e273455d4e7a1c6c29474f
jinghai <jinghai.xiao@gmail.com>
2017-02-24 15:07:48 +0800

    电影院-临时

--- c_dzdp/conf.js
+++ c_dzdp/conf.js
@@ -5,7 +5,7 @@
 var conf = {
     //【秒】【分】【小时】【日】【月】【周】 *所有 ?不指定 -区间 */5每5
     //cron:"00 00 08 1 3,9 *",//每年3月1日和9月1日
-    logLevel: 'debug',//debug|verbose|info|warn|error
+    logLevel: 'info',//debug|verbose|info|warn|error
     dbUrl: "mongodb://192.168.2.67/crawler",//'mongodb://user:pass@localhost:port,anotherhost:port,yetanother:port/mydatabase'
     crawler: {
         //url:'http://www.dianping.com/citylist/citylist?citypage=1',
