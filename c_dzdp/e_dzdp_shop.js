71bf389315b535aed1b494e57cb089bbdae7ada5
jinghai <jinghai.xiao@gmail.com>
2017-03-03 12:16:00 +0800

    增加文件队列

--- c_dzdp/e_dzdp_shop.js
+++ c_dzdp/e_dzdp_shop.js
@@ -12,6 +12,7 @@ var extractor = {
     target: [
         {
             test: function (url) {
+                //http://www.dianping.com/search/category/1/55/g6844
                 //剔除多余网址，含：m n o # 符号的不爬
                 var t = url.split('/');
                 var str = t[t.length - 1];
