6d74ecef8065ff3e41d3f64e9dac5b089ca7a019
jinghai <jinghai.xiao@gmail.com>
2017-04-05 12:07:23 +0800

    su

--- c_anjuke/conf.js
+++ c_anjuke/conf.js
@@ -4,7 +4,7 @@
  */
 var conf = {
     //【秒】【分】【小时】【日】【月】【周】 *所有 ?不指定 -区间 */5每5
-    cron:"00 00 08 1,10,20 * *",//每月1号,10号,20号早上8:30
+    cron:"00 00 08 1 * *",//每月1号,10号,20号早上8:30
     logLevel: 'info',//debug|verbose|info|warn|error
     dbUrl: "mongodb://192.168.2.67/crawler",//'mongodb://user:pass@localhost:port,anotherhost:port,yetanother:port/mydatabase'
     //dbUrl: "mongodb://192.168.2.56/crawler",
